9ea4b229eeac09bf21854e21af68ba4aa12d3b4d
go2665 <77224762+go2665@users.noreply.github.com>
2023-05-19 10:44:49 +0900

    Console - 생성자

--- 01_Console/01_Console/Character.cs
+++ 01_Console/01_Console/Character.cs
@@ -12,6 +12,26 @@ namespace _01_Console
         private float hp = 100.0f;
         private float exp = 0.0f;
 
+        // 기본 생성자
+        public Character() 
+        {
+            level = 1;
+            hp = 100.0f;
+            exp = 0.0f;
+        }
+
+        // hp만 설정하는 생성자
+
+
+        // level과 hp를 설정하는 생성자
+        public Character(int level, float hp)  
+        {
+            this.level = level; // 맴버인 level에 파라메터로 받은 level을 복사해라.
+            this.hp = hp;
+            exp = 0.0f;
+        }
+
+
         public void Attack()
         {
             Console.WriteLine("공격한다.");
