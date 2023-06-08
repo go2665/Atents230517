3e7372d01a99c0d1e5cc62792da4f9d7f28040a0
go2665 <77224762+go2665@users.noreply.github.com>
2023-06-08 10:14:11 +0900

    Shooting - 유도 미사일(일정 반경 안에 들어가면 유도 정지)

--- 02_Shooting/Assets/Scripts/Enemy/EnemyGuidedMissle.cs
+++ 02_Shooting/Assets/Scripts/Enemy/EnemyGuidedMissle.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class EnemyGuidedMissle : EnemyBase
 {
     Transform target;
+    bool onGuided = true;
 
     public override void OnInitialize()
     {
         target = GameManager.Inst.Player.transform;
+        onGuided = true;
     }
 
     protected override void OnMoveUpdate(float deltaTime)
     {
         base.OnMoveUpdate(deltaTime);
 
-        Vector3 dir = target.position - transform.position;       
-        
-        transform.right = -Vector3.Lerp(-transform.right, dir, deltaTime * 0.5f);
+        if (onGuided)
+        {
+            Vector3 dir = target.position - transform.position;
+
+            // transform의 오른쪽 방향 벡터를 -dir로 설정한다 => transform의 왼쪽 방향 벡터를 dir로 설정한다 
+            // => 거기에 맞는 회전을 한다.
+            //transform.right = -dir;
+            transform.right = -Vector3.Lerp(-transform.right, dir, deltaTime * 0.5f);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,6 +34,14 @@ public class EnemyGuidedMissle : EnemyBase
         {
             Die();
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            onGuided = false;
+        }        
     }
 
     // 미사일이 어느 정도 플레이어에게 근접하면 더이상 추적을 하지 않는다.
