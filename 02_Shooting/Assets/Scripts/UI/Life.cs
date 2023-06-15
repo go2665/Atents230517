78d9f29b13ae05c9aafb75b347b41a47ce0675df
go2665 <77224762+go2665@users.noreply.github.com>
2023-06-15 12:54:57 +0900

    Shooting - Life 패널 스크립트 완료

--- 02_Shooting/Assets/Scripts/UI/Life.cs
+++ 02_Shooting/Assets/Scripts/UI/Life.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Life : MonoBehaviour
 {
     // 플레이어의 HP가 변경되면 UI에서 변경해서 보여주기
+    TextMeshProUGUI lifeText;
+
+    private void Awake()
+    {
+        Transform textTransform = transform.GetChild(2);
+        lifeText = textTransform.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        // 플레이어의 onLifeChange가 발동되면 실행할 람다식 연결
+        GameManager.Inst.Player.onLifeChange += (life) => lifeText.text = life.ToString();
+    }
 }
