6aefb358087aa2e8b299928865f4380210467e32
go2665 <77224762+go2665@users.noreply.github.com>
2023-06-22 13:20:27 +0900

    3D Basic - 기즈모로 발사각과 가능 여부 그리기

--- 03_3D_Basic/Assets/Scripts/Enemy/TurretTrace.cs
+++ 03_3D_Basic/Assets/Scripts/Enemy/TurretTrace.cs
@@ -8,13 +8,34 @@ using UnityEditor;
 
 public class TurretTrace : TurretBase
 {
+    /// <summary>
+    /// 터렛의 시야 범위
+    /// </summary>
     public float sightRange = 10.0f;
+
+    /// <summary>
+    /// 터렛의 머리가 회전하는 속도
+    /// </summary>
     public float turnSpeed = 2.0f;
+
+    /// <summary>
+    /// 발사각
+    /// </summary>
     public float fireAngle = 10.0f;
 
+    /// <summary>
+    /// 현재 발사 중인지 아닌지 표시. true면 발사중, false면 발사하고 있지 않음
+    /// </summary>
     bool isFiring = false;
 
+    /// <summary>
+    /// 공격 대상(플레이어)
+    /// </summary>
     Transform target;
+
+    /// <summary>
+    /// 시야 범위 감지용 트리거
+    /// </summary>
     SphereCollider sightTrigger;
 
     protected override void Awake()
@@ -25,19 +46,20 @@ public class TurretTrace : TurretBase
 
     private void Start()
     {
-        sightTrigger.radius = sightRange;
+        sightTrigger.radius = sightRange;   // 시작할 때 sightRange만큼 트리거 크기 조정
     }
 
     private void Update()
     {
-        LookTarget();
+        // 대상이 있는지 확인해서 있으면 해당 방향으로 고개를 돌리다 범위안에 들어오면 공격 시작
+        LookTargetAndAttack();   
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if( other.CompareTag("Player") )
         {
-            target = other.transform;
+            target = other.transform;   // 플레이어가 트리거 안에 들어오면 타겟으로 지정
         }
     }
 
@@ -45,36 +67,64 @@ public class TurretTrace : TurretBase
     {
         if(other.CompareTag("Player"))
         {
-            target = null;
+            target = null;  // 플레이어가 트리거에서 나가면 타겟 해제
+            StopFire();     // 발사 중이면 발사도 중지
         }
     }    
 
-    void LookTarget()
+    /// <summary>
+    /// 대상을 바라보고 공격 범위안에 들어있으면 공격하는 함수
+    /// </summary>
+    void LookTargetAndAttack()
     {
-        // target을 바라보는 함수
-
-        if( target != null )
-        {
-            Vector3 dir = target.position - barrelBodyTransform.position;
+        if( target != null )    // 타겟이 있을 때만
+        {   
+            Vector3 dir = target.position - barrelBodyTransform.position;   // 방향 계산하고
             dir.y = 0;
             //barrelBodyTransform.rotation = Quaternion.LookRotation(dir);    // 특정 방향을 바라보는 회전을 만드는 함수
             //barrelBodyTransform.LookAt(target);   // 트랜스폼이 특정 지점을 바라보게 만드는 함수
 
+            // 천천히 회전 시키기
             barrelBodyTransform.rotation = Quaternion.Slerp(
                 barrelBodyTransform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * turnSpeed);
 
             // Vector3.Angle : 두 벡터가 이루는 사이각 중 작은 것을 리턴
             // Vector3.SignedAngle : 두 벡터의 사이각을 구하는데 축이되는 벡터를 기준으로 계산
 
+            // 목표지점까지의 각도 계산
             float angle = Vector3.Angle(barrelBodyTransform.forward, dir);
             if (angle < fireAngle)
             {
-                if (!isFiring)
-                {
-                    StartCoroutine(fireCoroutine);
-                    isFiring = true;
-                }
-            }            
+                StartFire();    // 발사각 안이면 발사
+            } 
+            else
+            {
+                StopFire();     // 밖이면 정지
+            }
+        }
+    }
+
+    /// <summary>
+    /// 총알을 발사 중이아니면 발사를 시작하는 함수
+    /// </summary>
+    void StartFire()
+    {
+        if (!isFiring)
+        {
+            StartCoroutine(fireCoroutine);
+            isFiring = true;
+        }
+    }
+
+    /// <summary>
+    /// 총알이 발사 중이면 발사를 멈추는 함수
+    /// </summary>
+    void StopFire()
+    {
+        if (isFiring)
+        {
+            StopCoroutine(fireCoroutine);
+            isFiring = false;
         }
     }
 
@@ -84,17 +134,28 @@ public class TurretTrace : TurretBase
         //Gizmos.DrawWireSphere(transform.position, sightRange);
         Handles.DrawWireDisc(transform.position, transform.up, sightRange, 2);
 
-        if(barrelBodyTransform == null)
+        if (barrelBodyTransform == null)
         {
             barrelBodyTransform = transform.GetChild(2);
         }
-        
+
+        Vector3 from = barrelBodyTransform.position;
+        Vector3 to;
+
         // 총알 나가는 선 그리기(시야범위까지만)
-        Gizmos.DrawLine(barrelBodyTransform.position, 
-            barrelBodyTransform.position + barrelBodyTransform.forward * sightRange);
+        to = barrelBodyTransform.position + barrelBodyTransform.forward * sightRange;
+        Gizmos.color = isFiring ? Color.red : Color.blue;   // 발사중이면 빨간색, 아니면 파란색
+        Gizmos.DrawLine(from, to);
+
+        Vector3 dir1 = Quaternion.AngleAxis(-fireAngle, barrelBodyTransform.up) * barrelBodyTransform.forward;
+        Vector3 dir2 = Quaternion.AngleAxis(fireAngle, barrelBodyTransform.up) * barrelBodyTransform.forward;
 
         // 발사각 그리기
-        // 총알 나가는 선의 색상은 발사각 안에 있을 때는 빨간색, 발사각 밖이면 파란색
+        Gizmos.color = Color.white;
+        to = barrelBodyTransform.position + dir1 * sightRange;
+        Gizmos.DrawLine(from, to);
+        to = barrelBodyTransform.position + dir2 * sightRange;
+        Gizmos.DrawLine(from, to);
     }
 #endif
 }
