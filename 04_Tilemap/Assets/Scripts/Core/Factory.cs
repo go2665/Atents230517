f2d3ff0de543b1163a27dc28d7e8dd6036fbd8ec
go2665 <77224762+go2665@users.noreply.github.com>
2023-07-04 09:31:27 +0900

    Tilemap - 프로젝트 생성

---
+++ 04_Tilemap/Assets/Scripts/Core/Factory.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PoolObjectType
+{
+    Bullet = 0,       // 총알
+}
+
+public class Factory : Singleton<Factory>
+{    
+    //BulletPool bulletPool;
+
+    protected override void OnInitialize()
+    {
+        base.OnInitialize();
+
+        //bulletPool = GetComponentInChildren<BulletPool>();
+
+        //bulletPool?.Initialize();
+    }
+
+    /// <summary>
+    /// 오브젝트를 풀에서 하나 가져오는 함수
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public GameObject GetObject(PoolObjectType type, Transform spawn = null)
+    {
+        GameObject result = null;
+        switch (type)
+        {
+            case PoolObjectType.Bullet:
+                //result = bulletPool?.GetObject(spawn)?.gameObject;                
+                break;
+            default:
+                result = new GameObject();
+                break;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// 오브젝트를 풀에서 하나 가져오면서 위치와 각도를 설정하는 함수
+    /// </summary>
+    /// <param name="type">생성할 오브젝트의 종류</param>
+    /// <param name="position">생성할 위치(월드좌표)</param>
+    /// <param name="angle">z축 회전 정도</param>
+    /// <returns>생성한 오브젝트</returns>
+    public GameObject GetObject(PoolObjectType type, Vector3 position, float angle = 0.0f)
+    {
+        GameObject obj = GetObject(type);
+        obj.transform.position = position;
+        obj.transform.Rotate(angle * Vector3.forward);
+
+        switch (type)
+        {        
+            default:                
+                break;
+        }
+        
+        return obj;
+    }
+}
