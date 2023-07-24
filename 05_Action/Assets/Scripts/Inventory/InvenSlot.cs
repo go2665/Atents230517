d7e269ab155cfa7a4551b16f9c0148c2d42b6992
go2665 <77224762+go2665@users.noreply.github.com>
2023-07-24 11:38:52 +0900

    Action - InvenSlot 거의 완료, Inventory 아이템 추가 중

--- 05_Action/Assets/Scripts/Inventory/InvenSlot.cs
+++ 05_Action/Assets/Scripts/Inventory/InvenSlot.cs
@@ -5,80 +5,179 @@ using UnityEngine;
 
 public class InvenSlot
 {
+    /// <summary>
+    /// 인벤토리에서의 인덱스
+    /// </summary>
     uint slotIndex;
+
+    /// <summary>
+    /// 인벤토리에서의 인덱스를 확인하기 위한 프로퍼티
+    /// </summary>
     public uint Index => slotIndex;
 
+    /// <summary>
+    /// 이 슬롯에 들어있는 아이템의 종류
+    /// </summary>
     ItemData slotItemData = null;
+
+    /// <summary>
+    /// 이 슬롯에 들어있는 아이템의 종류를 확인하기 위한 프로퍼티(쓰기는 private)
+    /// </summary>
     public ItemData ItemData
     {
         get => slotItemData;
         private set
         {
-            if (slotItemData != value)
+            if (slotItemData != value)      // 종류가 변경될 때만
             {
-                slotItemData = value;
-                onSlotItemChange?.Invoke();
+                slotItemData = value;       // 변경 작업 처리
+                onSlotItemChange?.Invoke(); // 아이템 종류가 변경되었다고 알람 보내기
             }
         }
     }
+    /// <summary>
+    /// 슬롯에 들어있는 아이템의 종류, 개수, 장비 여부가 변경되었다고 알리는 델리게이트
+    /// </summary>
     public Action onSlotItemChange;
 
+    /// <summary>
+    /// 슬롯에 아이템이 있는지 없는지 확인하는 프로퍼티(true면 비어있고, false면 아이템이 들어있다.)
+    /// </summary>
     public bool IsEmpty => slotItemData == null;
 
+    /// <summary>
+    /// 이 슬롯에 들어있는 아이템 개수
+    /// </summary>
     uint itemCount = 0;
+
+    /// <summary>
+    /// 아이템 개수를 확인하기 위한 프로퍼티(set은 private)
+    /// </summary>
     public uint ItemCount
     {
         get => itemCount;
         private set
         {
-            if(itemCount != value)
+            if(itemCount != value)              // 개수의 변경이 있을 때만 처리
             {
                 itemCount = value;
-                onSlotItemChange?.Invoke();
+                onSlotItemChange?.Invoke();     // 아이템 개수의 변경이 있었다고 알람 보내기
             }
         }
     }
 
+    /// <summary>
+    /// 이 슬롯의 아이템이 장비되었는지 여부
+    /// </summary>
     bool isEquipped = false;
+
+    /// <summary>
+    /// 이 슬롯의 장비여부를 확인하기 위한 프로퍼티
+    /// </summary>
     public bool IsEquipped
     {
         get => isEquipped;
         set
         {
-            IsEquipped = value;
-            onSlotItemChange?.Invoke();
+            isEquipped = value;         
+            onSlotItemChange?.Invoke(); // 무조건 변경되었다고 알림
         }
     }
 
+    /// <summary>
+    /// 생성자
+    /// </summary>
+    /// <param name="index">이 슬롯의 인덱스(인벤토리에서 몇번째 슬롯인지)</param>
     public InvenSlot(uint index)
     {
-        slotIndex = index;
+        slotIndex = index;      // slotIndex는 이후로 절대 변하면 안된다.
         ItemCount = 0;
         IsEquipped = false;
     }
 
-    public void AssignSlotItem(ItemData itemData, uint count = 1)
+    /// <summary>
+    /// 이 슬롯에 아이템을 설정하는 함수
+    /// </summary>
+    /// <param name="data">설정할 아이템 종류</param>
+    /// <param name="count">설정할 아이템 개수(set 용도, 추가되는 것이 아님)</param>
+    public void AssignSlotItem(ItemData data, uint count = 1)
     {
+        if( data != null )
+        {
+            ItemData = data;
+            ItemCount = count;
+            IsEquipped = false;
+            Debug.Log($"인벤토리 {slotIndex}번 슬롯에 \"{ItemData.itemName}\" 아이템이 {ItemCount}개 설정");
+        }
+        else
+        {
+            ClearSlotItem();    // data가 null이면 해당 슬롯은 초기화
+        }
     }
 
+    /// <summary>
+    /// 이 슬롯을 비우는 함수
+    /// </summary>
     public void ClearSlotItem()
     {
+        ItemData = null;
+        ItemCount = 0;
+        IsEquipped = false;
+        Debug.Log($"인벤토리 {slotIndex}번 슬롯을 비웁니다.");
     }
 
+    /// <summary>
+    /// 이 슬롯에 아이템 개수를 증가시키는 함수
+    /// </summary>
+    /// <param name="overCount">(출력용)추가하다가 넘친 개수</param>
+    /// <param name="increaseCount">증가시킬 개수</param>
+    /// <returns>성공여부(true면 안넘치고 다 추가 되서 성공, false면 넘쳤다)</returns>
     public bool IncreaseSlotItem(out uint overCount, uint increaseCount = 1)
     {
         bool result;
-        
-        // 나중에 제거할 코드
-        result = false;
-        overCount = 1;
-        // -----------------
+        int over;
+
+        uint newCount = ItemCount + increaseCount;
+        over = (int)newCount - (int)ItemData.maxStackCount;
+
+        if( over > 0 )
+        {
+            // 넘쳤다.
+            ItemCount = ItemData.maxStackCount;
+            overCount = (uint)over;
+            result = false;
+            Debug.Log($"인벤토리 {slotIndex}번 슬롯에 \"{ItemData.itemName}\" 아이템이 최대치까지 증가. 현재 {ItemCount}개. {over}개 넘침");
+        }
+        else
+        {
+            // 안넘쳤다.
+            ItemCount = newCount;
+            overCount = 0;
+            result = true;
+            Debug.Log($"인벤토리 {slotIndex}번 슬롯에 \"{ItemData.itemName}\" 아이템이 {increaseCount}개 증가. 현재 {ItemCount}개.");
+        }
 
         return result;
     }
 
+    /// <summary>
+    /// 이 슬롯에 아이템 개수 감소시키는 함수
+    /// </summary>
+    /// <param name="decreaseCount">감소시킬 아이템 개수</param>
     public void DecreaseSlotItem(uint decreaseCount = 1)
     {
+        int newCount = (int)ItemCount - (int)decreaseCount;
+        if(newCount < 1 )
+        {
+            // 슬롯이 완전히 비는 경우
+            ClearSlotItem();
+        }
+        else
+        {
+            // 슬롯에 아이템이 남아있는 경우
+            ItemCount = (uint)newCount;
+            Debug.Log($"인벤토리 {slotIndex}번 슬롯에 \"{ItemData.itemName}\" 아이템이 {decreaseCount}개 감소. 현재 {ItemCount}개.");
+        }
     }
 
     public void UseItem(GameObject target)
--- 05_Action/Assets/Scripts/Inventory/Inventory.cs
+++ 05_Action/Assets/Scripts/Inventory/Inventory.cs
@@ -67,4 +67,80 @@ public class Inventory
         itemDataManager = GameManager.Inst.ItemData;    // 아이템 데이터 메니저 캐싱
         this.owner = owner;                             // 소유자 기록
     }
+
+    /// <summary>
+    /// 인벤토리에 아이템을 하나 추가하는 함수
+    /// </summary>
+    /// <param name="code">추가할 아이템 종류</param>
+    /// <returns>true면 추가 성공, false면 추가 실패</returns>
+    public bool AddItem(ItemCode code)
+    {
+        bool result = false;
+        ItemData data = itemDataManager[code];
+
+        InvenSlot sameDataSlot = FindSameItem(data);
+        if(sameDataSlot != null)
+        {
+            // 같은 종류의 아이템이 있다.
+            // 아이템 개수 1 증가시키기고 결과 받기
+            result = sameDataSlot.IncreaseSlotItem(out uint _);  // 넘치는 개수가 의미 없어서 따로 받지 않음
+        }
+        else
+        {
+            // 같은 종류의 아이템이 없다.
+            InvenSlot emptySlot = FindEmptySlot();
+            if(emptySlot != null)
+            {
+                emptySlot.AssignSlotItem(data); // 빈슬롯이 있으면 아이템 하나 할당
+                result = true;
+            }
+            else
+            {
+                // 비어있는 슬롯이 없다.
+                Debug.Log("아이템 추가 실패 : 인벤토리가 가득 차있습니다.");
+            }
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// 인벤토리의 특정 슬롯에 아이템을 하나 추가하는 함수
+    /// </summary>
+    /// <param name="code"></param>
+    /// <param name="slotIndex"></param>
+    /// <returns></returns>
+    public bool AddItem(ItemCode code, uint slotIndex)
+    {
+        bool result = false;
+
+        return result;
+    }
+
+
+    // 인벤토리에서 아이템을 일정 개수만큼 제거하는 함수
+    // 인벤토리에서 아이템을 삭제하는 함수
+    // 인벤토리를 전부 비우는 함수
+    // 인벤토리의 특정 위치에서 다른 위치로 아이템을 이동시키는 함수
+    // 인벤토리에서 아이템을 일정량 덜어내어 임시 슬롯으로 보내는 함수
+    // 인벤토리를 정렬하는 함수
+
+    /// <summary>
+    /// 인벤토리에 파라메터와 같은 종류의 아이템이 들어있는 슬롯을 찾는 함수
+    /// </summary>
+    /// <param name="data">찾을 아이템 종류</param>
+    /// <returns>같은 종류의 아이템이 인벤토리에 없으면 null, 있으면 null이 아닌 값</returns>
+    InvenSlot FindSameItem(ItemData data)
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// 인벤토리에서 비어있는 슬롯을 찾는 함수
+    /// </summary>
+    /// <returns>비어있는 슬롯(첫번째)</returns>
+    InvenSlot FindEmptySlot()
+    {
+        return null;
+    }
 }
