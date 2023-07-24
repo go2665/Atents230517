7eeaad58506b1019cf4a1ca2f86eb697936ad6a0
go2665 <77224762+go2665@users.noreply.github.com>
2023-07-24 12:56:44 +0900

    Action - Inventory 아이템 추가/삭제 완료

--- 05_Action/Assets/Scripts/Inventory/Inventory.cs
+++ 05_Action/Assets/Scripts/Inventory/Inventory.cs
@@ -83,7 +83,7 @@ public class Inventory
         {
             // 같은 종류의 아이템이 있다.
             // 아이템 개수 1 증가시키기고 결과 받기
-            result = sameDataSlot.IncreaseSlotItem(out uint _);  // 넘치는 개수가 의미 없어서 따로 받지 않음
+            result = sameDataSlot.IncreaseSlotItem(out _);  // 넘치는 개수가 의미 없어서 따로 받지 않음
         }
         else
         {
@@ -107,32 +107,112 @@ public class Inventory
     /// <summary>
     /// 인벤토리의 특정 슬롯에 아이템을 하나 추가하는 함수
     /// </summary>
-    /// <param name="code"></param>
-    /// <param name="slotIndex"></param>
+    /// <param name="code">추가할 아이템의 종류</param>
+    /// <param name="slotIndex">아이템을 추가할 인덱스</param>
     /// <returns></returns>
     public bool AddItem(ItemCode code, uint slotIndex)
     {
         bool result = false;
 
+        if( IsValidIndex(slotIndex) )   // 인덱스가 적절한지 확인
+        {
+            ItemData data = itemDataManager[code];  // 아이템 데이터 가져오기
+            InvenSlot slot = slots[slotIndex];      // 아이템을 추가할 슬롯 가져오기
+            if(slot.IsEmpty)
+            {
+                slot.AssignSlotItem(data);          // 슬롯이 비었으면 아이템 할당
+            }
+            else
+            {
+                // 슬롯에 아이템이 이미 있는데
+                if(slot.ItemData == data)           // 아이템이 같은 종류이면
+                {
+                    result = slot.IncreaseSlotItem(out _);  // 아이템 증가
+                }
+                else
+                {
+                    // 다른 종류이면 실패
+                    Debug.Log($"아이템 추가 실패 : 인벤토리 {slotIndex}번 슬롯에 다른 아이템이 들어있습니다.");
+                }
+            }
+        }
+        else
+        {
+            // 인덱스가 잘못된 경우도 실패
+            Debug.Log($"아이템 추가 실패 : {slotIndex}번은 없는 인덱스입니다.");
+        }
+
         return result;
     }
 
+    /// <summary>
+    /// 인벤토리에서 아이템을 일정 개수만큼 감소시키는 함수
+    /// </summary>
+    /// <param name="slotIndex">아이템을 감소시킬 슬롯의 인덱스</param>
+    /// <param name="decreaseCount">감소시킬 개수</param>
+    public void RemoveItem(uint slotIndex, uint decreaseCount = 1) 
+    {
+        if(IsValidIndex(slotIndex) )
+        {
+            InvenSlot invenSlot = slots[slotIndex];
+            invenSlot.DecreaseSlotItem(decreaseCount);
+        }
+        else
+        {
+            Debug.Log($"아이템 감소 실패 : {slotIndex}는 없는 인덱스입니다.");
+        }
+    }
 
-    // 인벤토리에서 아이템을 일정 개수만큼 제거하는 함수
-    // 인벤토리에서 아이템을 삭제하는 함수
-    // 인벤토리를 전부 비우는 함수
+    /// <summary>
+    /// 인벤토리에서 아이템을 삭제하는 함수
+    /// </summary>
+    /// <param name="slotIndex">아이템을 삭제할 슬롯의 인덱스</param>
+    public void ClearSlot(uint slotIndex)
+    {
+        if (IsValidIndex(slotIndex))
+        {
+            InvenSlot invenSlot = slots[slotIndex];
+            invenSlot.ClearSlotItem();
+        }
+        else
+        {
+            Debug.Log($"아이템 삭제 실패 : {slotIndex}는 없는 인덱스입니다.");
+        }
+    }
+
+    /// <summary>
+    /// 인벤토리를 전부 비우는 함수
+    /// </summary>
+    public void ClearInventory()
+    {
+        foreach(var slot in slots)
+        {
+            slot.ClearSlotItem();
+        }
+    }
+
     // 인벤토리의 특정 위치에서 다른 위치로 아이템을 이동시키는 함수
     // 인벤토리에서 아이템을 일정량 덜어내어 임시 슬롯으로 보내는 함수
     // 인벤토리를 정렬하는 함수
 
     /// <summary>
-    /// 인벤토리에 파라메터와 같은 종류의 아이템이 들어있는 슬롯을 찾는 함수
+    /// 인벤토리에 파라메터와 같은 종류의 아이템이 들어있는 슬롯(여유공간이 있는 슬롯)을 찾는 함수
     /// </summary>
     /// <param name="data">찾을 아이템 종류</param>
     /// <returns>같은 종류의 아이템이 인벤토리에 없으면 null, 있으면 null이 아닌 값</returns>
     InvenSlot FindSameItem(ItemData data)
     {
-        return null;
+        InvenSlot findSlot = null;
+        foreach(var slot in slots)  // 모든 슬롯을 다 돌면서
+        {
+            if (slot.ItemData == data && slot.ItemCount < slot.ItemData.maxStackCount)  // itemData가 같고 여유 공간이 있으면 찾았다.
+            {
+                findSlot = slot;
+                break;
+            }
+        }
+
+        return findSlot;
     }
 
     /// <summary>
@@ -141,6 +221,32 @@ public class Inventory
     /// <returns>비어있는 슬롯(첫번째)</returns>
     InvenSlot FindEmptySlot()
     {
-        return null;
+        InvenSlot findSlot = null;
+        foreach (var slot in slots)     // 모든 슬롯을 다 돌면서
+        {
+            if(slot.IsEmpty)            // 비어있는 슬롯이 있으면 찾았다.
+            {
+                findSlot = slot;
+                break;
+            }
+        }
+
+        return findSlot;
+    }
+
+    /// <summary>
+    /// 적절한 인덱스인지 확인하는 함수
+    /// </summary>
+    /// <param name="index">확인할 인덱스</param>
+    /// <returns>true면 적절한 인덱스, false면 없는 인덱스</returns>
+    bool IsValidIndex(uint index) => (index < SlotCount) || (index == TempSlotIndex);
+
+    /// <summary>
+    /// 테스트용 : 인벤토리 안의 내용을 콘솔창에 출력하는 함수
+    /// </summary>
+    public void PrintInventory()
+    {
+        // 예시
+        // [ 루비(1/3), 사파이어(1/5), 에메랄드(2/5), (빈칸), (빈칸), (빈칸) ]
     }
 }
---
+++ 05_Action/Assets/Scripts/Test/Test_Inventory.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_Inventory : TestBase
+{
+    public uint size = 6;
+    public ItemCode code = ItemCode.Ruby;
+    public uint index = 0;
+
+    Inventory inven;
+
+    private void Start()
+    {
+        inven = new Inventory(null, size);
+        inven.AddItem(code, 0);
+        inven.AddItem(code, 0);
+        inven.AddItem(code, 0);
+        inven.AddItem(code, 1);
+        inven.AddItem(code, 1);
+        inven.AddItem(code, 2);
+        inven.AddItem(code, 3);
+        inven.AddItem(code, 4);
+        inven.AddItem(code, 5);
+    }
+
+    protected override void Test1(InputAction.CallbackContext context)
+    {
+        inven.AddItem(code);
+    }
+
+    protected override void Test2(InputAction.CallbackContext context)
+    {
+        inven.AddItem(code, index);
+    }
+
+    protected override void Test3(InputAction.CallbackContext context)
+    {
+        inven.RemoveItem(index);
+    }
+
+    protected override void Test4(InputAction.CallbackContext context)
+    {
+        inven.ClearSlot(index);
+    }
+
+    protected override void Test5(InputAction.CallbackContext context)
+    {
+        inven.ClearInventory();
+    }
+}
