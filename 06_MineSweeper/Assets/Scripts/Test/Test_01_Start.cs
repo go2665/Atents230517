9a867726ac27b655be38f316af49cdb37bda2124
go2665 <77224762+go2665@users.noreply.github.com>
2023-08-10 14:19:07 +0900

    MineSweeper - 폰트 파일 추가, 기본 스크립트 추가

---
+++ 06_MineSweeper/Assets/Scripts/Test/Test_01_Start.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_01_Start : MonoBehaviour
+{    
+}
+
+// 필요 클래스
+//  - Cell
+//  - Board
+//  - Reset
+//  - ImageNumber
+//      - Timer
+//      - FlagCounter
