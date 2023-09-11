7c033af262d096ac529a5ae8399b8726dd6acee1
go2665 <77224762+go2665@users.noreply.github.com>
2023-09-11 13:38:14 +0900

    Network - GridPainter 사전 준비

---
+++ 08_BoardGame/Assets/Scripts/Board/GridPainter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPainter : MonoBehaviour
+{
+    public GameObject linePrefab;
+    public GameObject letterPrefab;
+
+}
