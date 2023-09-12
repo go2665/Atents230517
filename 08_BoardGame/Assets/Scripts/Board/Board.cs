aede67aa1706e75e4b02540e1d3b36386fde938d
go2665 <77224762+go2665@users.noreply.github.com>
2023-09-12 13:05:28 +0900

    Network - 보드 유틸리티 함수들 구현

--- 08_BoardGame/Assets/Scripts/Board/Board.cs
+++ 08_BoardGame/Assets/Scripts/Board/Board.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Board : MonoBehaviour
 {
@@ -10,13 +11,21 @@ public class Board : MonoBehaviour
     public const int BoardSize = 10;
 
     /// <summary>
+    /// 인덱스가 범위를 벗어났다는 표시
+    /// </summary>
+    public const int NOT_VALID_INDEX = -1;
+
+    /// <summary>
     /// 월드 좌표를 그리드 좌표로 변경해주는 함수
     /// </summary>
     /// <param name="worldPos">월드 좌표</param>
     /// <returns>그리드 좌표</returns>
     public Vector2Int WorldToGrid(Vector3 worldPos)
     {
-        return Vector2Int.zero;
+        worldPos.y = transform.position.y;
+
+        Vector3 diff = worldPos - transform.position;
+        return new Vector2Int(Mathf.FloorToInt(diff.x), Mathf.FloorToInt(-diff.z));
     }
 
     /// <summary>
@@ -27,7 +36,7 @@ public class Board : MonoBehaviour
     /// <returns>해당 그리드의 가운데 지점 월드좌표</returns>
     public Vector3 GridToWorld(int x, int y)
     {
-        return Vector3.zero;
+        return transform.position + new Vector3(x + 0.5f, 0, -(y+0.5f));
     }
 
     /// <summary>
@@ -37,7 +46,7 @@ public class Board : MonoBehaviour
     /// <returns>해당 그리드의 가운데 지점 월드좌표</returns>
     public Vector3 GridToWorld(Vector2Int grid)
     {
-        return Vector3.zero;
+        return GridToWorld(grid.x, grid.y);
     }    
 
     /// <summary>
@@ -47,16 +56,19 @@ public class Board : MonoBehaviour
     /// <returns>인덱스에 해당하는 그리드의 월드 좌표</returns>
     public Vector3 IndexToWorld(int index)
     {
-        return Vector3.zero;
+        return GridToWorld(IndexToGrid(index));
     }
 
     /// <summary>
     /// 마우스 커서 위치의 그리드좌표를 구해주는 함수
     /// </summary>
     /// <returns>마우스 커서 위치의 그리드좌표</returns>
-    public Vector2Int GetMouseGridPosision()
-    { 
-        return Vector2Int.zero; 
+    public Vector2Int GetMouseGridPosition()
+    {
+        Vector2 screenPos = Mouse.current.position.ReadValue();
+        Vector3 world = Camera.main.ScreenToWorldPoint(screenPos);
+
+        return WorldToGrid(world);
     }
 
     /// <summary>
@@ -64,9 +76,60 @@ public class Board : MonoBehaviour
     /// </summary>
     /// <param name="worldPos">확인할 월드 좌표</param>
     /// <returns>true면 보드 안쪽, false면 보드 바깥쪽</returns>
-    public bool IsValidPosition(Vector3 worldPos)
+    public bool IsInBoard(Vector3 worldPos)
     {
-        return false;
+        worldPos.y = transform.position.y;
+
+        Vector3 diff = worldPos - transform.position;
+
+        return diff.x >= 0.0f && diff.x <= BoardSize && diff.z <= 0 && diff.z >= -BoardSize;
     }
 
+    /// <summary>
+    /// 인덱스 값을 그리드 좌표로 변경해주는 함수
+    /// </summary>
+    /// <param name="index">인덱스 값</param>
+    /// <returns>결과 그리드 좌표</returns>
+    public static Vector2Int IndexToGrid(int index)
+    {
+        return new Vector2Int(index % BoardSize, index / BoardSize);
+    }
+
+    /// <summary>
+    /// 그리드 좌표를 인덱스 값으로 변경해주는 함수
+    /// </summary>
+    /// <param name="x">그리드x</param>
+    /// <param name="y">그리드y</param>
+    /// <returns>해당 위치의 인덱스 값</returns>
+    public static int GridToIndex(int x, int y)
+    {
+        int result = NOT_VALID_INDEX;   // 적절하지 않은 경우 -1
+        if (IsVaildGridPosition(x,y))
+        {
+            result = x + y * BoardSize;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// 그리드 좌표가 적절한지 확인하는 함수
+    /// </summary>
+    /// <param name="x">x좌표</param>
+    /// <param name="y">y좌표</param>
+    /// <returns>true면 적절하고 false면 보드 밖의 좌표</returns>
+    public static bool IsVaildGridPosition(int x, int y)
+    {
+        return x > -1 && x < BoardSize && y > -1 && y <BoardSize;
+    }
+
+    /// <summary>
+    /// 그리드 좌표가 적절한지 확인하는 함수
+    /// </summary>
+    /// <param name="grid">그리드 좌표</param>
+    /// <returns>true면 적절하고 false면 보드 밖의 좌표</returns>
+    public static bool IsValidGridPosition(Vector2Int grid)
+    {
+        return IsVaildGridPosition(grid.x, grid.y);
+    }
 }
--- 08_BoardGame/Assets/Scripts/Test/Test_02_Board.cs
+++ 08_BoardGame/Assets/Scripts/Test/Test_02_Board.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Test_02_Board : TestBase
 {
-    // 실습
-    // 1. Board 클래스의 함수들 완성하기
-    // 2. 마우스를 클릭했을 때 클릭한 지점의 그리드 좌표 출력하기
-    // 3. 마우스를 클릭했을 때 클릭한 지점이 보드 안인지 밖인지 출력하기
-    // 4. 마우스를 클릭했을 때 클릭한 지점의 그리드 중점 출력하기
+    public Board board;
+
+    protected override void TestClick(InputAction.CallbackContext context)
+    {
+        Vector2 screenPos = Mouse.current.position.ReadValue();
+        Vector3 world = Camera.main.ScreenToWorldPoint(screenPos);
+        //Vector2Int grid = board.WorldToGrid(world);
+        Vector2Int grid = board.GetMouseGridPosition();
+        Debug.Log($"그리드 좌표 : ({grid.x},{grid.y})");
+
+        if( board.IsInBoard(world) )
+        {
+            Debug.Log("보드의 안쪽입니다.");
+        }
+        else
+        {
+            Debug.Log("보드의 바깥쪽입니다.");
+        }
+
+        Vector3 center = board.GridToWorld(grid);
+        Debug.Log($"이 그리드의 중점 : ({center.x}, {center.y}, {center.z})");
+    }
 }
