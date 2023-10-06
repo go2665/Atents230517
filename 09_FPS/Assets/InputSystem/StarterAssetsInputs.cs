ce323b9754cdef20b8a7d9e1c916c9beb1357345
go2665 <77224762+go2665@users.noreply.github.com>
2023-10-06 12:13:15 +0900

    FPS - PlayerCapsule 주석 추가, 샘플 에셋 추가

--- 09_FPS/Assets/InputSystem/StarterAssetsInputs.cs
+++ 09_FPS/Assets/InputSystem/StarterAssetsInputs.cs
@@ -8,17 +8,17 @@ namespace StarterAssets
 	public class StarterAssetsInputs : MonoBehaviour
 	{
 		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+		public Vector2 move;		// 이동 입력 현황
+		public Vector2 look;		// 시야 회전 현황
+		public bool jump;			// 점프 버튼 현황
+		public bool sprint;			// 달리기 모드 현황
 
 		[Header("Movement Settings")]
-		public bool analogMovement;
+		public bool analogMovement;	// 아날로그 스틱이 있는지 여부(true면 있다, false면 없다)
 
 		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
+		public bool cursorLocked = true;		// 커서락 기능을 사용할지 여부(락이 되면 마우스커서가 안보인다)
+		public bool cursorInputForLook = true;	// 커서 입력을 카메라 회전용으로 사용
 
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 		public void OnMove(InputValue value)
@@ -66,13 +66,22 @@ namespace StarterAssets
 			sprint = newSprintState;
 		}
 		
+		/// <summary>
+		/// 응용프로그램에 포커스가 가면 실행되는 함수
+		/// </summary>
+		/// <param name="hasFocus">true면 포커스가 갔다. false면 포커스가 나갔다.</param>
 		private void OnApplicationFocus(bool hasFocus)
 		{
-			SetCursorState(cursorLocked);
+			SetCursorState(cursorLocked);	// 상태 변경
 		}
 
+		/// <summary>
+		/// 커서의 락상태를 변경하는 함수
+		/// </summary>
+		/// <param name="newState">true면 락을 한다, false면 락을 해제한다.</param>
 		private void SetCursorState(bool newState)
 		{
+			// 상태가 lock이되면 커서는 안보이게 되고 항상 가운데에 있다고 가정한다.
 			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
 		}
 	}
--- 09_FPS/Assets/Scripts/Sample/BasicRigidBodyPush.cs
+++ 09_FPS/Assets/Scripts/Sample/BasicRigidBodyPush.cs
@@ -1,35 +1,61 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class BasicRigidBodyPush : MonoBehaviour
 {
+	/// <summary>
+	/// 밀 대상의 레이어 설정
+	/// </summary>
 	public LayerMask pushLayers;
+	
+	/// <summary>
+	/// 부딪친 대상을 밀 수 있는지 설정
+	/// </summary>
 	public bool canPush;
-	[Range(0.5f, 5f)] public float strength = 1.1f;
 
+	/// <summary>
+	/// 미는 힘
+	/// </summary>
+	[Range(0.5f, 5f)] public float strength = 1.1f;	
+		
+	/// <summary>
+	/// CharactorController가 이동하다가 충돌이 발생하면 실행되는 이벤트 함수(OnCollisionEnter는 실행안됨)
+	/// </summary>
+	/// <param name="hit">충돌 정보</param>
 	private void OnControllerColliderHit(ControllerColliderHit hit)
 	{
-		if (canPush) PushRigidBodies(hit);
+        // canPush가 true일 때만 동작
+        if (canPush) PushRigidBodies(hit);
 	}
 
-	private void PushRigidBodies(ControllerColliderHit hit)
+	/// <summary>
+	/// 대상을 미는 함수
+	/// </summary>
+	/// <param name="hit">충돌 정보</param>
+    private void PushRigidBodies(ControllerColliderHit hit)
 	{
 		// https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
 
-		// make sure we hit a non kinematic rigidbody
-		Rigidbody body = hit.collider.attachedRigidbody;
+		// 리지드바디가 없거나 키네마틱으로 설정되어 있으면 종료
+		Rigidbody body = hit.collider.attachedRigidbody;	// 충돌한 대상의 리지드바디 저장
 		if (body == null || body.isKinematic) return;
 
-		// make sure we only push desired layer(s)
-		var bodyLayerMask = 1 << body.gameObject.layer;
-		if ((bodyLayerMask & pushLayers.value) == 0) return;
+        // body.gameObject.layer;		// ex) 레이어가 11번째이면 10이 기록되었다.
 
-		// We dont want to push objects below us
+        // 1		=			0000 0000 0000 0000 0000 0000 0000 0001
+        // 1 << 10	=			0000 0000 0000 0000 0000 0100 0000 0000
+        // pushLayers.value =	0000 0000 0000 0000 0000 0100 0000 0000
+
+        // 설정한 레이어가 아니면 종료
+        int bodyLayerMask = 1 << body.gameObject.layer;		
+		if ((bodyLayerMask & pushLayers.value) == 0) return;	// &결과가 0이면 서로 다른 레이어
+
+		// 내가 너무 아래쪽으로 밀고 있으면 종료
 		if (hit.moveDirection.y < -0.3f) return;
 
-		// Calculate push direction from move direction, horizontal motion only
+		// 대상을 밀 방향 계산하기(이동 방향대로 밀기, y는 무시)
 		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
 
-		// Apply the push and take strength into account
+		// 계산한 방향으로 힘을 가하기
 		body.AddForce(pushDir * strength, ForceMode.Impulse);
 	}
 }
