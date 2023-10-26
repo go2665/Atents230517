78288eb6be4bc317e8fa132785fd7c874acc160d
go2665 <77224762+go2665@users.noreply.github.com>
2023-10-26 12:14:07 +0900

    FPS - 적 스폰 준비

---
+++ 09_FPS/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // 1. 플레이어의 총에 맞으면 죽는다.
+}
---
+++ 09_FPS/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // 1. 적을 생성
+    // 2. 항상 생성되어 있는 적은 10명 유지
+    //  2.1. 시작할 때 10마리 생성
+    //  2.2. 플레이어가 적을 죽이면 플레이어로 부터 떨어진 랜덤한 위치에 1초 뒤 생성
+}
